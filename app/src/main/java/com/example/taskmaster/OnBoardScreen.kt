c5409afc0c27633e23ddf564f639280c1599740e
Sriharan <sriharan029@gmail.com>
2024-05-07 16:37:03 +0530

    initial commit with OnBoardScreen

---
+++ app/src/main/java/com/example/taskmaster/OnBoardScreen.kt
@@ -0,0 +1,11 @@
+package com.example.taskmaster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoardScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_board_screen)
+    }
+}
